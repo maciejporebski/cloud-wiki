337c714d57a48d3b11df691c3ddcfcd703d10846
Maciej Porebski <macporebski@gmail.com>
2023-09-13 11:14:02 +0100

    Change token to secure string

--- src/microsoft-graph/code/Connect-ToGraphUsingAzSession.ps1
+++ src/microsoft-graph/code/Connect-ToGraphUsingAzSession.ps1
@@ -1,1 +1,5 @@
+# On older versions of Microsoft.Graph use:
 Connect-MgGraph -AccessToken (Get-AzAccessToken -ResourceTypeName MSGraph).Token
+
+# On latest version of Microsoft.Graph use:
+Connect-MgGraph -AccessToken (ConvertTo-SecureString -String (Get-AzAccessToken -ResourceTypeName MSGraph).Token -AsPlainText)

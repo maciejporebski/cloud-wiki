328fc7b02c39cf35cbf6367d456bb14c166b8115
Maciej Porebski <macporebski@outlook.com>
2025-09-19 09:29:17 +0100

    Update Connect-ToGraphUsingAzSession.ps1

--- src/microsoft-graph/code/Connect-ToGraphUsingAzSession.ps1
+++ src/microsoft-graph/code/Connect-ToGraphUsingAzSession.ps1
@@ -1,5 +1,1 @@
-# On older versions of Microsoft.Graph use:
-Connect-MgGraph -AccessToken (Get-AzAccessToken -ResourceTypeName MSGraph).Token
-
-# On latest version of Microsoft.Graph use:
-Connect-MgGraph -AccessToken (ConvertTo-SecureString -String (Get-AzAccessToken -ResourceTypeName MSGraph).Token -AsPlainText)
+Connect-MgGraph -AccessToken ((Get-AzAccessToken -ResourceTypeName MSGraph).Token)
